2b9dadcda636de06da805dc54c5dc372a032c7c7
naveen <naveenvadaga@gmail.com>
2020-07-08 09:08:05 +0530

    added authentication

--- sof_backend/settings.py
+++ sof_backend/settings.py
@@ -37,9 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
     'questions',
     'user_auth',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
